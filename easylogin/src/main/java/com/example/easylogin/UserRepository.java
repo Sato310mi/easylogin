be7405d386d2b9e2a4d2f22c4a1f65e48c48fbc8
Sato310mi <ko103www@gmail.com>
2023-09-04 15:42:45 +0900

    Lesson_easylogin

---
+++ easylogin/src/main/java/com/example/easylogin/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.easylogin.model.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	List<User> findByUserNameAndPassword(String userName, String password);
+}
